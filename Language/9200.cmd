10d73db43e7dcadd65cb165a7055f8883e045730
test user <test@example.com>
2025-05-09 23:56:03 +0900

    모든 변경 사항 커밋

---
+++ Language/9200.cmd
@@ -0,0 +1,57 @@
+@echo off
+title Downloader
+if not exist "9200.7z.001" (
+  echo 9200_Patch.7z.001 ^(1/11^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/9200/9200.7z.001','9200.7z.001'^);
+  cls
+)
+if not exist "9200.7z.002" (
+  echo 9200_Patch.7z.001 ^(2/11^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/9200/9200.7z.002','9200.7z.002'^);
+  cls
+)
+if not exist "9200.7z.003" (
+  echo 9200_Patch.7z.001 ^(3/11^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/9200/9200.7z.003','9200.7z.003'^);
+  cls
+)
+if not exist "9200.7z.004" (
+  echo 9200_Patch.7z.001 ^(4/11^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/9200/9200.7z.004','9200.7z.004'^);
+  cls
+)
+if not exist "9200.7z.005" (
+  echo 9200_Patch.7z.001 ^(5/11^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/9200/9200.7z.005','9200.7z.005'^);
+  cls
+)
+if not exist "9200.7z.006" (
+  echo 9200_Patch.7z.001 ^(6/11^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/9200/9200.7z.006','9200.7z.006'^);
+  cls
+)
+if not exist "9200.7z.007" (
+  echo 9200_Patch.7z.001 ^(7/11^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/9200/9200.7z.007','9200.7z.007'^);
+  cls
+)
+if not exist "9200.7z.008" (
+  echo 9200_Patch.7z.001 ^(8/11^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/9200/9200.7z.008','9200.7z.008'^);
+  cls
+)
+if not exist "9200.7z.009" (
+  echo 9200_Patch.7z.001 ^(9/11^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/9200/9200.7z.009','9200.7z.009'^);
+  cls
+)
+if not exist "9200.7z.010" (
+  echo 9200_Patch.7z.001 ^(10/11^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/9200/9200.7z.010','9200.7z.010'^);
+  cls
+)
+if not exist "9200.exe" (
+  echo 9200_Patch.7z.001 ^(11/11^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/9200/9200.exe','9200.exe'^);
+  cls
+)
