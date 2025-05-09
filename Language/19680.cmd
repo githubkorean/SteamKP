b9f4527091f25c35ee199f00863ea4230df46ce7
test user <test@example.com>
2025-05-10 00:11:06 +0900

    모든 변경 사항 커밋

---
+++ Language/19680.cmd
@@ -0,0 +1,17 @@
+@echo off
+title Downloader
+if not exist "19680.7z.001" (
+  echo 9200_Patch.7z.001 ^(1/3^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/19680/19680.7z.001','19680.7z.001'^);
+  cls
+)
+if not exist "19680.7z.002" (
+  echo 9200_Patch.7z.001 ^(2/3^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/19680/19680.7z.002','19680.7z.002'^);
+  cls
+)
+if not exist "19680.exe" (
+  echo 9200_Patch.7z.001 ^(3/3^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/19680/19680.exe','19680.exe'^);
+  cls
+)
