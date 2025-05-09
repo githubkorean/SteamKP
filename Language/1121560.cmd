a77e72d147739051df1c0150b655aaf4b2fdc844
test user <test@example.com>
2025-05-10 01:23:19 +0900

    모든 변경 사항 커밋

--- Language/1121560.cmd
+++ Language/1121560.cmd
@@ -6,27 +6,27 @@ if not exist "1121560.7z.001" (
   cls
 )
 if not exist "1121560.7z.002" (
-  echo 1121560.7z.001 ^(2/6^)
+  echo 1121560.7z.002 ^(2/6^)
   PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1121560/1121560.7z.002','1121560.7z.002'^);
   cls
 )
 if not exist "1121560.7z.003" (
-  echo 1121560.7z.001 ^(3/6^)
+  echo 1121560.7z.003 ^(3/6^)
   PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1121560/1121560.7z.003','1121560.7z.003'^);
   cls
 )
 if not exist "1121560.7z.004" (
-  echo 1121560.7z.001 ^(4/6^)
+  echo 1121560.7z.004 ^(4/6^)
   PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1121560/1121560.7z.004','1121560.7z.004'^);
   cls
 )
 if not exist "1121560.7z.005" (
-  echo 1121560.7z.001 ^(5/6^)
+  echo 1121560.7z.005 ^(5/6^)
   PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1121560/1121560.7z.005','1121560.7z.005'^);
   cls
 )
 if not exist "1121560.exe" (
-  echo 1121560.7z.001 ^(6/6^)
+  echo 1121560.exe ^(6/6^)
   PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1121560/1121560.exe','1121560.exe'^);
   cls
 )
--- Language/65790.cmd
+++ Language/65790.cmd
@@ -1,32 +1,32 @@
 @echo off
 title Downloader
 if not exist "65790.7z.001" (
-  echo 9200_Patch.7z.001 ^(1/6^)
+  echo 65790.7z.001 ^(1/6^)
   PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/65790/65790.7z.001','65790.7z.001'^);
   cls
 )
 if not exist "65790.7z.002" (
-  echo 9200_Patch.7z.001 ^(2/6^)
+  echo 65790.7z.002 ^(2/6^)
   PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/65790/65790.7z.002','65790.7z.002'^);
   cls
 )
 if not exist "65790.7z.003" (
-  echo 9200_Patch.7z.001 ^(3/6^)
+  echo 65790.7z.003 ^(3/6^)
   PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/65790/65790.7z.003','65790.7z.003'^);
   cls
 )
 if not exist "65790.7z.004" (
-  echo 9200_Patch.7z.001 ^(4/6^)
+  echo 65790.7z.004 ^(4/6^)
   PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/65790/65790.7z.004','65790.7z.004'^);
   cls
 )
 if not exist "65790.7z.005" (
-  echo 9200_Patch.7z.001 ^(5/6^)
+  echo 65790.7z.005 ^(5/6^)
   PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/65790/65790.7z.005','65790.7z.005'^);
   cls
 )
 if not exist "65790.exe" (
-  echo 9200_Patch.7z.001 ^(6/6^)
+  echo 65790.exe ^(6/6^)
   PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/65790/65790.exe','65790.exe'^);
   cls
 )
