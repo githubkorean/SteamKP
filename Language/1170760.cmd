bd177408da541551ff8834e9c2c0b8ea97c2f92f
ㅤ <>
2025-06-16 21:32:20 +0900

    ㅤ

---
+++ Language/1170760.cmd
@@ -0,0 +1,242 @@
+@echo off
+title Downloader
+if not exist "1170760.7z.001" (
+  echo 1170760.7z.001 ^(1/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.001','1170760.7z.001'^);
+  cls
+)
+if not exist "1170760.7z.002" (
+  echo 1170760.7z.002 ^(2/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.002','1170760.7z.002'^);
+  cls
+)
+if not exist "1170760.7z.003" (
+  echo 1170760.7z.003 ^(3/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.003','1170760.7z.003'^);
+  cls
+)
+if not exist "1170760.7z.004" (
+  echo 1170760.7z.004 ^(4/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.004','1170760.7z.004'^);
+  cls
+)
+if not exist "1170760.7z.005" (
+  echo 1170760.7z.005 ^(5/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.005','1170760.7z.005'^);
+  cls
+)
+if not exist "1170760.7z.006" (
+  echo 1170760.7z.006 ^(6/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.006','1170760.7z.006'^);
+  cls
+)
+if not exist "1170760.7z.007" (
+  echo 1170760.7z.007 ^(7/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.007','1170760.7z.007'^);
+  cls
+)
+if not exist "1170760.7z.008" (
+  echo 1170760.7z.008 ^(8/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.008','1170760.7z.008'^);
+  cls
+)
+if not exist "1170760.7z.009" (
+  echo 1170760.7z.009 ^(9/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.009','1170760.7z.009'^);
+  cls
+)
+if not exist "1170760.7z.010" (
+  echo 1170760.7z.010 ^(10/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.010','1170760.7z.010'^);
+  cls
+)
+if not exist "1170760.7z.011" (
+  echo 1170760.7z.011 ^(11/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.011','1170760.7z.011'^);
+  cls
+)
+if not exist "1170760.7z.012" (
+  echo 1170760.7z.012 ^(12/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.012','1170760.7z.012'^);
+  cls
+)
+if not exist "1170760.7z.013" (
+  echo 1170760.7z.013 ^(13/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.013','1170760.7z.013'^);
+  cls
+)
+if not exist "1170760.7z.014" (
+  echo 1170760.7z.014 ^(14/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.014','1170760.7z.014'^);
+  cls
+)
+if not exist "1170760.7z.015" (
+  echo 1170760.7z.015 ^(15/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.015','1170760.7z.015'^);
+  cls
+)
+if not exist "1170760.7z.016" (
+  echo 1170760.7z.016 ^(16/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.016','1170760.7z.016'^);
+  cls
+)
+if not exist "1170760.7z.017" (
+  echo 1170760.7z.017 ^(17/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.017','1170760.7z.017'^);
+  cls
+)
+if not exist "1170760.7z.018" (
+  echo 1170760.7z.018 ^(18/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.018','1170760.7z.018'^);
+  cls
+)
+if not exist "1170760.7z.019" (
+  echo 1170760.7z.019 ^(19/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.019','1170760.7z.019'^);
+  cls
+)
+if not exist "1170760.7z.020" (
+  echo 1170760.7z.020 ^(20/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.020','1170760.7z.020'^);
+  cls
+)
+if not exist "1170760.7z.021" (
+  echo 1170760.7z.021 ^(21/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.021','1170760.7z.021'^);
+  cls
+)
+if not exist "1170760.7z.022" (
+  echo 1170760.7z.022 ^(22/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.022','1170760.7z.022'^);
+  cls
+)
+if not exist "1170760.7z.023" (
+  echo 1170760.7z.023 ^(23/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.023','1170760.7z.023'^);
+  cls
+)
+if not exist "1170760.7z.024" (
+  echo 1170760.7z.024 ^(24/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.024','1170760.7z.024'^);
+  cls
+)
+if not exist "1170760.7z.025" (
+  echo 1170760.7z.025 ^(25/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.025','1170760.7z.025'^);
+  cls
+)
+if not exist "1170760.7z.026" (
+  echo 1170760.7z.026 ^(26/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.026','1170760.7z.026'^);
+  cls
+)
+if not exist "1170760.7z.027" (
+  echo 1170760.7z.027 ^(27/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.027','1170760.7z.027'^);
+  cls
+)
+if not exist "1170760.7z.028" (
+  echo 1170760.7z.028 ^(28/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.028','1170760.7z.028'^);
+  cls
+)
+if not exist "1170760.7z.029" (
+  echo 1170760.7z.029 ^(29/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.029','1170760.7z.029'^);
+  cls
+)
+if not exist "1170760.7z.030" (
+  echo 1170760.7z.030 ^(30/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.030','1170760.7z.030'^);
+  cls
+)
+if not exist "1170760.7z.031" (
+  echo 1170760.7z.031 ^(31/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.031','1170760.7z.031'^);
+  cls
+)
+if not exist "1170760.7z.032" (
+  echo 1170760.7z.032 ^(32/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.032','1170760.7z.032'^);
+  cls
+)
+if not exist "1170760.7z.033" (
+  echo 1170760.7z.033 ^(33/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.033','1170760.7z.033'^);
+  cls
+)
+if not exist "1170760.7z.034" (
+  echo 1170760.7z.034 ^(34/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.034','1170760.7z.034'^);
+  cls
+)
+if not exist "1170760.7z.035" (
+  echo 1170760.7z.035 ^(35/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.035','1170760.7z.035'^);
+  cls
+)
+if not exist "1170760.7z.036" (
+  echo 1170760.7z.036 ^(36/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.036','1170760.7z.036'^);
+  cls
+)
+if not exist "1170760.7z.037" (
+  echo 1170760.7z.037 ^(37/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.037','1170760.7z.037'^);
+  cls
+)
+if not exist "1170760.7z.038" (
+  echo 1170760.7z.038 ^(38/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.038','1170760.7z.038'^);
+  cls
+)
+if not exist "1170760.7z.039" (
+  echo 1170760.7z.039 ^(39/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.039','1170760.7z.039'^);
+  cls
+)
+if not exist "1170760.7z.040" (
+  echo 1170760.7z.040 ^(40/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.040','1170760.7z.040'^);
+  cls
+)
+if not exist "1170760.7z.041" (
+  echo 1170760.7z.041 ^(41/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.041','1170760.7z.041'^);
+  cls
+)
+if not exist "1170760.7z.042" (
+  echo 1170760.7z.042 ^(42/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.042','1170760.7z.042'^);
+  cls
+)
+if not exist "1170760.7z.043" (
+  echo 1170760.7z.043 ^(43/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.043','1170760.7z.043'^);
+  cls
+)
+if not exist "1170760.7z.044" (
+  echo 1170760.7z.044 ^(44/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.044','1170760.7z.044'^);
+  cls
+)
+if not exist "1170760.7z.045" (
+  echo 1170760.7z.045 ^(45/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.045','1170760.7z.045'^);
+  cls
+)
+if not exist "1170760.7z.046" (
+  echo 1170760.7z.046 ^(46/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.046','1170760.7z.046'^);
+  cls
+)
+if not exist "1170760.7z.047" (
+  echo 1170760.7z.047 ^(47/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.7z.047','1170760.7z.047'^);
+  cls
+)
+if not exist "1170760.exe" (
+  echo 1170760.exe ^(48/48^)
+  PowerShell ^(New-Object System.Net.WebClient^).DownloadFile^('https://raw.githubusercontent.com/githubkorean/SteamKP/refs/heads/main/Language/1170760/1170760.exe','1170760.exe'^);
+  cls
+)
